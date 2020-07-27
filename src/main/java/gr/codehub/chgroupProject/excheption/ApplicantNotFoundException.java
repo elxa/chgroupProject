f08452f1bc43fec1ede5251b0199b855c6e441c3
ilianapapadimitriou <ilianapapadimitirou.ceid@gmail.com>
2020-07-27 17:56:54 +0300

    my_first_commit

---
+++ src/main/java/gr/codehub/chgroupProject/excheption/ApplicantNotFoundException.java
@@ -0,0 +1,7 @@
+package gr.codehub.chgroupProject.excheption;
+
+public class ApplicantNotFoundException extends Exception{
+    public ApplicantNotFoundException(String message) {
+        super(message);
+    }
+}
