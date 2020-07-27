5a9722b0d6bc315a288943b1b501a7409b817bba
elxa <52540608+elxa@users.noreply.github.com>
2020-07-27 15:27:32 +0300

    findApplicantByFirstNameAndLastName

--- src/main/java/gr/codehub/chgroupProject/repository/SkillRepository.java
+++ src/main/java/gr/codehub/chgroupProject/repository/SkillRepository.java
@@ -1,9 +1,8 @@
 package gr.codehub.chgroupProject.repository;
 
+import gr.codehub.chgroupProject.model.ApplicantSkill;
 import gr.codehub.chgroupProject.model.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface SkillRepository extends JpaRepository<Skill, Integer> {
 }
