74d605b7251770421ea337842f30616b0001c7e0
Laers Tsoukos <tsouko.laers@outlook.com.gr>
2020-07-27 13:28:24 +0300

    "Create ApplicantSkill Service-ServiceImpl-match1-Repo and SkillRepo"

--- src/main/java/gr/codehub/chgroupProject/controller/match1Skill.java
+++ src/main/java/gr/codehub/chgroupProject/controller/match1Skill.java
@@ -3,8 +3,8 @@ package gr.codehub.chgroupProject.controller;
 import gr.codehub.chgroupProject.model.ApplicantSkill;
 import gr.codehub.chgroupProject.service.ApplicantSkillService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +14,8 @@ public class match1Skill {
     private ApplicantSkillService applicantSkillService;
 
     @PostMapping("applicant/{applicantId}/skill/{skillId}")
-    public ApplicantSkill addApplicantSkill(@RequestBody int applicantId,
-                                            @RequestBody int skillId)
+    public ApplicantSkill addApplicantSkill(@PathVariable int applicantId,
+                                            @PathVariable int skillId)
     //TODO throws ApplicntCreationException, SkillCreationException
     {
         return applicantSkillService.addApplicantSkill(applicantId,skillId);
