e68ae5a57e0cc5900f3e143fd63eab13e5c6cdf5
Laers Tsoukos <tsouko.laers@outlook.com.gr>
2020-07-31 15:43:08 +0300

    "Correct JUnit Test with MockitoTest"

--- src/main/java/gr/codehub/chgroupProject/service/JobOfferSkillService.java
+++ src/main/java/gr/codehub/chgroupProject/service/JobOfferSkillService.java
@@ -5,6 +5,7 @@ import gr.codehub.chgroupProject.exception.SkillNotFoundException;
 import gr.codehub.chgroupProject.model.JobOfferSkill;
 
 public interface JobOfferSkillService {
-    JobOfferSkill addJobOfferSkill(int jobOfferId, int skillId) throws SkillNotFoundException, JobOfferNotFoundException;
+    JobOfferSkill addJobOfferSkill(int jobOfferId, int skillId)
+            throws SkillNotFoundException, JobOfferNotFoundException;
 
 }
